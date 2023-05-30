dd1e74418dcee1bbb14a3c8f5bd0a075592ed665
Caleb <cmk265@cornell.edu>
2023-05-30 15:39:31 -0400

    version settings in json form

--- run-full.py
+++ run-full.py
@@ -4,7 +4,13 @@ import argparse
 
 
 def main():
-    with open("version-settings.json") as f:
+    parser = argparse.ArgumentParser(description="Process args for resource estimates")
+    parser.add_argument("-s", "--sequential", action="store_true")
+    parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("-j", "--json")
+    parser.add_argument("-v", "--version", default="version-settings/current.json")
+    args = parser.parse_args()
+    with open(args.version) as f:
         json_dict = json.load(f)
         # calyx/dahlia commit to use
         calyx_version = json_dict.get("calyx_version", "master")
@@ -15,21 +21,14 @@ def main():
         subprocess.run(
             ["./configure-fud.sh", calyx_version, dahlia_version, fud_calyx_extension]
         )
+
+        cmdline_args = ["python3.9", "estimates.py"]
         # probably a better way to do this, but want run-full.py to take some of
         # the same cmdline arguments that estimates.py uses
-        parser = argparse.ArgumentParser(
-            description="Process args for resource estimates"
-        )
-        parser.add_argument("-s", "--sequential", action="store_true")
-        parser.add_argument("-d", "--debug", action="store_true")
-        parser.add_argument("-j", "--json")
-        args = parser.parse_args()
-        cmdline_args = ["python3.9", "estimates.py"]
         for k, v in vars(args).items():
             if v is True:
                 cmdline_args.append("--" + k)
             elif k == "json" and v is not None:
-                print(v)
                 cmdline_args.append("-j")
                 cmdline_args.append(f"""{v}""")
         # run estimates.py
