33be276ff2bf123f83c28c1de8de6ef518fbc11f
calebmkim <calebkim707@gmail.com>
2024-04-02 07:39:18 -0400

    sc sh results

--- backup/scripts/polybench_calyx.sh
+++ backup/scripts/polybench_calyx.sh
@@ -6,29 +6,29 @@ else
     parallelism="-j1"
 fi
 
-# SH->SC (25 minutes)
-mkdir -p results/standard/futil-sh-sc
-ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to resource-estimate > results/standard/futil-sh-sc/{/.}.json"
+# # SH->SC (25 minutes)
+# mkdir -p results/standard/futil-sh-sc
+# ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to resource-estimate > results/standard/futil-sh-sc/{/.}.json"
 
-# SH->SC latency (7 minutes)
-mkdir -p results/standard/futil-sh-sc-latency
-ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to dat --through verilog -s verilog.data '{}.data' | jq '{ "latency": .cycles }' > results/standard/futil-sh-sc-latency/{/.}.json"
+# # SH->SC latency (7 minutes)
+# mkdir -p results/standard/futil-sh-sc-latency
+# ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to dat --through verilog -s verilog.data '{}.data' | jq '{ "latency": .cycles }' > results/standard/futil-sh-sc-latency/{/.}.json"
 
 # SC (25 minutes)
 mkdir -p results/standard/futil-sc
-ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to resource-estimate -s calyx.flags "-d cell-share" > results/standard/futil-sc/{/.}.json"
+ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to resource-estimate -s calyx.flags '-d cell-share' > results/standard/futil-sc/{/.}.json"
 
 # SC (7 minutes)
 mkdir -p results/standard/futil-sc-latency
-ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to dat --through verilog -s verilog.data '{}.data' -s calyx.flags "-d cell-share" | jq '{ "latency": .cycles }' > results/standard/futil-sc-latency/{/.}.json"
+ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to dat --through verilog -s verilog.data '{}.data' -s calyx.flags '-d cell-share' | jq '{ "latency": .cycles }' > results/standard/futil-sc-latency/{/.}.json"
 
 # SH (25 minutes)
 mkdir -p results/standard/futil-sh
-ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to resource-estimate -s calyx.flags "-x static-promotion:compaction=false" > results/standard/futil-sh/{/.}.json"
+ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to resource-estimate -s calyx.flags '-x static-promotion:compaction=false' > results/standard/futil-sh/{/.}.json"
 
 # SH (7 minutes)
 mkdir -p results/standard/futil-sh-latency
-ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to dat --through verilog -s verilog.data '{}.data' -s calyx.flags "-x static-promotion:compaction=false" | jq '{ "latency": .cycles }' > results/standard/futil-sh-latency/{/.}.json"
+ls benchmarks/polybench/*.fuse | parallel --bar $parallelism "fud e -q {} --to dat --through verilog -s verilog.data '{}.data' -s calyx.flags '-x static-promotion:compaction=false' | jq '{ "latency": .cycles }' > results/standard/futil-sh-latency/{/.}.json"
 
 # # unrolled (25 minutes)
 # mkdir -p results/unrolled/futil
