440d8239d1fa68792dfa40fb4d5c2b9e49dd9ab2
calebmkim <calebkim707@gmail.com>
2024-04-20 11:25:52 -0400

    systolic cleaning

--- paper-results/systolic/drive.py
+++ paper-results/systolic/drive.py
@@ -6,7 +6,6 @@ import os
 if __name__ == "__main__":
     """
     Simulates systolic array based on input json settings.
-    See `run-settings/*.json` for sample input json settings.
     """
     parser = argparse.ArgumentParser(description="Process some json file names")
     parser.add_argument("-j", "--json", type=str)
@@ -15,40 +14,39 @@ if __name__ == "__main__":
     with open(args.json) as f:
         json_dict = json.load(f)
 
-    calyx_flags = json_dict.get("calyx_flags", "")
-    output_dir = json_dict["output_dir"]
-    output_path = f"systolic-results/{output_dir}"
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
+    fud_target = json_dict["target"]
+    fud_flags = json_dict["fud_flags"]
+    # dirs lets us know which directory to look for the benchmarks, and
+    # which directory we should put the results in.
+    dirs = json_dict["dirs"]
 
-    # Cloning calyx twice & setting up fud is too much work
-    # subprocess.run(
-    #     ["sh", "configure-fud.sh", calyx_stage_name, json_dict["commit_hash"]]
-    # )
-    for size in json_dict["sizes"]:
-        target = json_dict["target"]
-        data_suffix = "banked" if json_dict["banked"] else "unbanked"
-        data_path = f"systolic-data/{size}-{data_suffix}.json"
-        input_path = f"{json_dict['input_dir']}/{size}.systolic"
-        output_path = f"systolic-results/{output_dir}/{size}.json"
-        fud_command = [
-            "fud",
-            "e",
-            "-q",
-            input_path,
-            "--to",
-            target,
-            "-s",
-            "verilog.data",
-            data_path,
-            "-s",
-            "calyx.flags",
-            calyx_flags,
-            "-o",
-            output_path,
-        ]
-        for fud_flag in json_dict["fud_flags"]:
-            fud_command += fud_flag
+    input_data_files = os.listdir("input-data")
 
-        print(f"Running fud command: {fud_command}")
-        subprocess.run(fud_command)
+    for dir_tuple in dirs:
+        source_dir = dir_tuple[0]
+        assert os.path.exists(source_dir), f"Invliad Source Directory: {source_dir}"
+        results_dir = dir_tuple[1]
+        # Make the results directory if it doesn't exist
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+        for source_file in os.listdir(source_dir):
+            source_path = os.path.join(source_dir, source_file)
+            results_path = os.path.join(results_dir, source_file + ".json")
+            fud_command = [
+                "fud",
+                "e",
+                "-q",
+                source_path,
+                "--to",
+                fud_target,
+                "-o",
+                results_path,
+            ]
+            for fud_flag in json_dict["fud_flags"]:
+                fud_command += fud_flag
+            if fud_target == "dat":
+                size = source_file.split(".")[0]
+                fud_command += ["-s", "verilog.data", f"input-data/{size}.json"]
+
+            print(f"Running fud command: {fud_command}")
+            # subprocess.run(fud_command)
