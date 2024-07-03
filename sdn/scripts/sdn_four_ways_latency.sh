4916288694770728e3b6473a0c54c6085912d6a5
calebmkim <calebkim707@gmail.com>
2024-07-03 16:24:20 -0400

    split sdn scripts

--- sdn/scripts/sdn_four_ways_latency.sh
+++ sdn/scripts/sdn_four_ways_latency.sh
@@ -8,25 +8,17 @@ data=benchmarks/sdn/sdn.data
 # By default, the code is promoted to Piezo. To disable promotion, we pass the -d static-promotion flag.
 
 # Unannotated code, not further promoted
-# Resource estimates
-fud e -q $code --to resource-estimate --from calyx -s calyx.flags ' -d static-promotion ' > results/no_ann_no_prom_resources.json
 # Cycle counts
 fud e -q $code --to dat --from calyx --through verilog -s verilog.data $data -s calyx.flags ' -d static-promotion ' | jq '{ "latency": .cycles }'  > results/no_ann_no_prom_cycles.json
 
 # Unannotated code, further promoted to Piezo
-# Resource estimates
-fud e -q $code --to resource-estimate --from calyx > results/no_ann_yes_prom_resources.json
 # Cycle counts
 fud e -q $code --to dat --from calyx --through verilog -s verilog.data $data | jq '{ "latency": .cycles }'  > results/no_ann_yes_prom_cycles.json
 
 # Annotated code, not further promoted
-# Resource estimates
-fud e -q $annotated_code --to resource-estimate --from calyx -s calyx.flags ' -d static-promotion ' > results/yes_ann_no_prom_resources.json
 # Cycle counts
 fud e -q $annotated_code --to dat --from calyx --through verilog -s verilog.data $data -s calyx.flags ' -d static-promotion '  | jq '{ "latency": .cycles }' > results/yes_ann_no_prom_cycles.json
 
 # Annotated code, further promoted to Piezo
-# Resource estimates
-fud e -q $annotated_code --to resource-estimate --from calyx > results/yes_ann_yes_prom_resources.json
 # Cycle counts
 fud e -q $annotated_code --to dat --from calyx --through verilog -s verilog.data $data | jq '{ "latency": .cycles }' > results/yes_ann_yes_prom_cycles.json
